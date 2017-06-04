bf437eb4f72a8c3b0621be66f5bb532410ea26d1
Angelo Chaves <angeloechaves@gmail.com>
2017-06-04 04:29:21 -0300

    Initial clip posting of mercy dancing

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ var path = require('path'),
       }
     },
     T = new Twit(config.twitter),
-    stream = T.stream('statuses/sample');
+    stream = T.stream('statuses/sample'),
+    fs = require('fs'),
+    ytdl = require('ytdl-core'),
+    ffmpeg = require('fluent-ffmpeg');
 
 app.use(express.static('public'));
 
@@ -31,6 +34,55 @@ app.all("/" + process.env.BOT_ENDPOINT, function (request, response) {
     else{
       resp.sendStatus(200);
     }
+  });
+});
+
+app.all("/" + process.env.BOT_ENDPOTINT_MERCY, function (request, response) {
+  /* This would post a video of mercy dancing */
+  var resp = response;
+  var output = path.resolve(__dirname, 'assets/audio/audio.mp4');
+  // Download a nice song from youtube
+  var yt = ytdl(process.env.NUNCA_ME_FALTES_YT, {filter: 'audioonly'})
+    .pipe(fs.createWriteStream(output))
+    .on('finish', () => {
+      // Append the audio to the mercy dancing video
+      // ffmpeg -i assets/video/mercy_noaudio.mp4 -i assets/audio/audio.mp4 -map 0:v -map 1:a -shortest -c:v libx264 -c:a libfdk_aac assets/video/mercy.mp4
+      var ffmpegCommand = ffmpeg()
+                            .addInput('assets/video/mercy_noaudio.mp4')
+                            .addInput('assets/audio/audio.mp4')
+                            .outputOptions(['-map 0:v','-map 1:a','-shortest'])
+                            .videoCodec('libx264')
+                            .audioCodec('libfdk_aac')
+                            .output('assets/video/mercy.mp4')
+                            .on('end', () => {
+                              T.postMediaChunked({ file_path: 'assets/video/mercy.mp4' }, function (err, data, response) {
+                                var mediaIdStr = data.media_id_string
+                                var meta_params = { media_id: mediaIdStr }
+
+                                T.post('media/metadata/create', meta_params, function (err, data, response) {
+                                  if (!err) {
+                                    // now we can reference the media and post a tweet (media will attach to the tweet)
+                                    var params = { status: '💃', media_ids: [mediaIdStr] }
+
+                                    T.post('statuses/update', params, function (err, data, response) {
+                                      if (err){
+                                        resp.sendStatus(500);
+                                        console.log('Error!');
+                                        console.log(err);
+                                      }
+                                      else{
+                                        resp.sendStatus(200);
+                                      }
+                                    })
+                                  } else {
+                                    resp.sendStatus(500);
+                                    console.log('Error!');
+                                    console.log(err);
+                                  }
+                                })
+                              });
+                            }).run();
+
   });
 });
 
