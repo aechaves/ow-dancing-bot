449f30e87f390f5c55dcf8615b6bacbee63ddd33
Angelo Chaves <angeloechaves@gmail.com>
2017-06-09 10:59:14 -0300

    Change timeout setting

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ var path = require('path'),
         consumer_key: process.env.CONSUMER_KEY,
         consumer_secret: process.env.CONSUMER_SECRET,
         access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+        timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
       }
     },
     T = new Twit(config.twitter),
