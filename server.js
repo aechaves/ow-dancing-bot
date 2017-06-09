d48dd6da393737f90e688c56eaea171c92aeffbc
Angelo Chaves <angeloechaves@gmail.com>
2017-06-09 11:06:13 -0300

    set timeout through env variables

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path = require('path'),
         consumer_secret: process.env.CONSUMER_SECRET,
         access_token: process.env.ACCESS_TOKEN,
         access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-        timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+        timeout_ms: process.env.TIMEOUT_MS,  // optional HTTP request timeout to apply to all requests.
       }
     },
     T = new Twit(config.twitter),
